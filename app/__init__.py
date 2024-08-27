0c3077de9426d9f62773910cbb2160dabf9b062b
JacTBB <jackiesoon66@gmail.com>
2024-08-28 04:07:05 +0800

    Upload project files

---
+++ app/__init__.py
@@ -0,0 +1,41 @@
+from flask import Flask
+from flask_login import LoginManager
+from app.config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf.csrf import CSRFProtect
+
+
+
+db = SQLAlchemy()
+csrf = CSRFProtect()
+
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+    
+    
+    from .models import Users
+
+    with app.app_context():
+        db.init_app(app)
+        db.create_all()
+        csrf.init_app(app)
+
+
+        login_manager = LoginManager()
+        login_manager.login_view = 'auth.login'
+        login_manager.init_app(app)
+
+        @login_manager.user_loader
+        def user_loader(id):
+            return Users.query.get(id)
+
+
+        from app.auth import auth
+        from app.views import views
+
+        app.register_blueprint(auth)
+        app.register_blueprint(views)
+
+    return app
